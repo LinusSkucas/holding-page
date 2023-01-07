b7c146e72a2a6eb3dca0743d3fc1c9f605d5e082
Linus Skucas <git@linus.sh>
2023-01-07 06:36:59 +0000

    Deal with autoplay not working

--- stuff.js
+++ stuff.js
@@ -13,3 +13,30 @@ function mover(response) {
 	}
 }
 httpGetAsync("https://RingedCurlyModes.linusskucas.repl.co", mover);
+
+const audio = new Audio("https://cloud-pba2emxos-hack-club-bot.vercel.app/0waiting-music-116216_audio.mp4");
+audio.repeat = true;
+audio.volume = 0;
+
+document.getElementById( 'btn' ).onclick = e => {
+  document.getElementById( 'splash' ).remove();
+  audio.play().then( () => {
+	audio.currentTime = 0;
+	audio.volume = 1;
+  });
+};
+
+
+function playAudio() {
+  console.log('playing');
+  audio.play();
+}
+function pauseAudio() {
+  console.log('pausing');
+  audio.pause();
+}
+function changeSrc() {
+  console.log('changing src');
+  audio.src = "https://dl.dropboxusercontent.com/s/okbnk1ycupxrrb2/_Sgt._Pepper%27s_Lonely_Hearts_Club_Band_by_the_Beatles_1967%20%281%29%20%28online-audio-converter.com%29.mp3";
+  audio.play();
+}
